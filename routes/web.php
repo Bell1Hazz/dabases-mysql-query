88fa488682971611d660ad49c6aa7cce8ec3186d
Abel <ridhosakti7@gmail.com>
2025-10-23 14:34:16 +0700

    update routing

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return redirect()->route('articles.index');
@@ -14,4 +15,9 @@ Route::resource('articles', ArticleController::class);
 Route::prefix('authors')->group(function () {
     Route::get('/', [AuthorController::class, 'index'])->name('authors.index');
     Route::get('/{user}', [AuthorController::class, 'show'])->name('authors.show');
+});
+// Comment Routes
+Route::prefix('articles/{article}/comments')->group(function () {
+    Route::post('/', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
