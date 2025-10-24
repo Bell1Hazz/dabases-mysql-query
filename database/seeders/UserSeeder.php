534267bd2d33ca1af7dd0c2a9cd341605134452e
Abel <ridhosakti7@gmail.com>
2025-10-24 10:32:56 +0700

    update Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,29 +12,44 @@ class UserSeeder extends Seeder
     {
         $users = [
             [
+                'name' => 'Admin ArticleHub',
+                'email' => 'admin@articlehub.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'bio' => 'System Administrator',
+            ],
+            [
                 'name' => 'Sarah Johnson',
                 'email' => 'sarah@articlehub.com',
                 'password' => Hash::make('password'),
+                'role' => 'author',
+                'bio' => 'Technology enthusiast and AI researcher',
             ],
             [
                 'name' => 'Michael Chen',
                 'email' => 'michael@articlehub.com',
                 'password' => Hash::make('password'),
+                'role' => 'author',
+                'bio' => 'UI/UX Designer passionate about user experiences',
             ],
             [
                 'name' => 'Emily Rodriguez',
                 'email' => 'emily@articlehub.com',
                 'password' => Hash::make('password'),
+                'role' => 'author',
+                'bio' => 'Business strategist and entrepreneur',
             ],
             [
                 'name' => 'David Kim',
                 'email' => 'david@articlehub.com',
                 'password' => Hash::make('password'),
+                'role' => 'author',
             ],
             [
                 'name' => 'Lisa Wang',
                 'email' => 'lisa@articlehub.com',
                 'password' => Hash::make('password'),
+                'role' => 'author',
             ],
         ];
 
