f5a7293a9a270b4a454cfbcfe4de8c99da862fc4
Abel <ridhosakti7@gmail.com>
2025-10-24 10:29:07 +0700

    setup auth

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
-    return redirect()->route('articles.index');
+    return view('welcome');
 });
 
-// Article Resource
-Route::resource('articles', ArticleController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Nested Comment Resource
-Route::resource('articles.comments', CommentController::class)->only(['store', 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Author Resource
-Route::resource('authors', AuthorController::class)->only(['index', 'show']);
+require __DIR__.'/auth.php';
